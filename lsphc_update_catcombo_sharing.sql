b0b3c70b32e49385bc90b0ffcb076886adf04bde
muhammad salihu <31661000+salihuM@users.noreply.github.com>
2025-02-04 11:28:13 +0100

    Add files via upload

---
+++ lsphc_update_catcombo_sharing.sql
@@ -0,0 +1,32 @@
+BEGIN;
+-- SQL to update sharing for category combos
+
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'iMpvzkLxbKA';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'yzv8MJxvTsE';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'eCPII23gfYb';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'GzhbhpYPfC9';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'Glk5fC9lIwa';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'thRcyhPOW4E';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'DbWtWjLE9nb';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'RDBYAo1Ddhi';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'XYLk7zhALxj';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'meHCkw0OfPf';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'Naz8TCH8y2S';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'KSTkxSofy8B';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'WumntCnz3yI';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'fjDsGxZaGiZ';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'r0ODkmJtoBX';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'FJGpgYWUu6H';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'vY6SUIprbGX';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'rNzARNRVpgh';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'ojp2gQluuaS';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'JCoOiQ19Q0q';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'sdRS6z6dVrv';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'kqjR5fOmRlg';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'EF1EcfewwKT';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'FcuCCpxzoQQ';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'yiAmigEZg1p';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'Dxz2BpK8om8';
+UPDATE categorycombo SET sharing = '{"owner": "Kw35FjsfJFe", "users": {}, "public": "rw------", "external": false, "userGroups": {}}' WHERE uid = 'kHa5eDwcpuU';
+
+COMMIT;
