be4381cd360d93f9f2c123a47c96f8154e588cb7
muhammad salihu <31661000+salihuM@users.noreply.github.com>
2025-02-14 16:55:50 +0100

    Create lsphc_update_coc_uid.sql

---
+++ lsphc_update_coc_uid.sql
@@ -0,0 +1,35 @@
+BEGIN;
+
+-- Update UIDs of rogue COCs
+
+update categoryoptioncombo set uid = 'qiPeu1fhUzc' where uid = 'a5lgj3ydhFW';
+update categoryoptioncombo set uid = 'zg4UEdnkjTe' where uid = 'bVwYTzsjSRa';
+update categoryoptioncombo set uid = 'nxOYlIH0fyC' where uid = 'W8dcY2Ju6tk';
+update categoryoptioncombo set uid = 'YLnVx36kEoB' where uid = 'Gy6BAr5lOhZ';
+update categoryoptioncombo set uid = 'spJcVru5xm5' where uid = 'B05YBMXa8pl';
+update categoryoptioncombo set uid = 'S1UIhPhfCJu' where uid = 'f6zXDpqiXcS';
+update categoryoptioncombo set uid = 'WeChkhdxd0J' where uid = 'WuUqYfbo8te';
+update categoryoptioncombo set uid = 'eTW0ydGTe78' where uid = 'OqAhvuJBKuN';
+update categoryoptioncombo set uid = 'WSz0ikY7Yy6' where uid = 'HFjenhKq01t';
+update categoryoptioncombo set uid = 'Agj0kWQNVcb' where uid = 'IStV3YPCSWY';
+update categoryoptioncombo set uid = 'BGiySNUZJ6L' where uid = 'iVBUBYizeNH';
+update categoryoptioncombo set uid = 'JDSQGLCxXZD' where uid = 'cay1vAU65xu';
+
+update categoryoptioncombo set uid = 'cKf6Y3d6XxG' where uid = 't4LlMj4zTpr';
+update categoryoptioncombo set uid = 'OSiqazly2Gf' where uid = 'pPQlPy4JNcu';
+update categoryoptioncombo set uid = 'UxYxduhZOMQ' where uid = 'zV3CPHtNGNj';
+update categoryoptioncombo set uid = 'P68rcU7ahzc' where uid = 'Hj79SIvY3a6';
+update categoryoptioncombo set uid = 'pLJb53AcTuj' where uid = 'PB8wuyxykvY';
+update categoryoptioncombo set uid = 'UOncmG7eakp' where uid = 'MlHFhHIErP5';
+update categoryoptioncombo set uid = 'VSa75IMnlKH' where uid = 'oj28GxTvpoH';
+update categoryoptioncombo set uid = 'gmW8MmLRJM9' where uid = 'sOQW6Y9WOZp';
+
+update categoryoptioncombo set uid = 'k1tblU2z2DR' where uid = 'iyr6aEosVik';
+update categoryoptioncombo set uid = 'nT4YcX1oqYa' where uid = 'd3HBieMpgcy';
+
+update categoryoptioncombo set uid = 'W06NzdJ52q0' where uid = 'RD2fpWzTtPN';
+update categoryoptioncombo set uid = 'vlpLmuuM3af' where uid = 'I20DD7m61Wp';
+update categoryoptioncombo set uid = 'twJft7xNxJF' where uid = 'qYaHvfUBpVZ';
+update categoryoptioncombo set uid = 'Mml2zeHwNRf' where uid = 'yGppGVZvXJM';
+
+COMMIT;
